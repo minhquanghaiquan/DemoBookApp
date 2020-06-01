0e120300dc4ccd2f987cab7a89eb9b3593f38583
minhquanghaiquan <minhquanghaiquan@gmail.com>
2020-06-01 18:42:26 +0200

    delete start

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 require('dotenv').config()
 //database
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://heroku_m3v5mzb9:hh0jj74crf20e78fte8hhmkgl1@ds027495.mlab.com:27495/heroku_m3v5mzb9', 
+mongoose.connect(process.env.MONGODB_URI, 
                  {useNewUrlParser: true, 
                   useUnifiedTopology: true});
 mongoose.connection.on('connected', ()=>{
